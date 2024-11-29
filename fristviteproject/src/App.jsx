3c1884e060feb56dbebcfc01e8810c91d6c92ed9
praveen choudhary <pc852180@gmail.com>
2024-11-29 12:26:38 +0530

    use java script xml

--- fristviteproject/src/App.jsx
+++ fristviteproject/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createElement, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // let a = createElement("h1",[],"hello from element");
+  //a
+  let name = "praveen"
+  let obj ={
+    fname:"praveen choudhary",
+    age:22,
+    course:"mern full stack"
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>my name is {name} </h1>
+      <h2>{obj.fname}</h2>
+      <h3>{obj.age}</h3>
+      <h4>{obj.course}</h4>
+     
+    </div>
   )
 }
 
