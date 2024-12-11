5e91066e3b93420f4d4e5c131083c6e64e8238fc
praveen choudhary <pc852180@gmail.com>
2024-12-11 18:57:21 +0530

    bootstarp slider

---
+++ bootstrapreact/src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./Hero"
+import Nav from "./navbar"
+
+
+function App() {
+ 
+
+  return (
+
+    <>
+    <Nav/>
+    <Hero/>
+    <h1 className="text-primary">Welcome Praveen</h1>
+     
+    </>
+  )
+}
+
+export default App
