37af7cb4cb0be3d83750a75f9ae0e40f428a055e
praveen choudhary <pc852180@gmail.com>
2024-12-14 12:44:51 +0530

    React Router Dom

---
+++ RounterReactproject/src/Home.jsx
@@ -0,0 +1,34 @@
+const Home = () => {
+  return (
+    <>
+      <h1>
+        Hello praveen your are selected for 40akh pakage from Gogal company
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At ducimus,
+        accusantium nisi dolore mollitia, dolorem quam iste accusamus eum
+        aperiam reprehenderit enim. Animi tempora facilis aperiam aliquid
+        corrupti quos id. Asperiores pariatur eligendi dignissimos officia,
+        velit ipsa cupiditate dolores ex laudantium earum, ut voluptatem, omnis
+        sequi obcaecati similique quo suscipit maxime ab laborum labore porro
+        explicabo incidunt! Cumque, excepturi reprehenderit? Dicta deserunt
+        dolore facilis corporis corrupti recusandae dolorum distinctio, alias ut
+        quis, commodi optio, nam placeat. Aperiam hic aliquid, suscipit vitae
+        laudantium reprehenderit facere atque accusamus corrupti fugiat
+        consequatur ex. Nam consequatur in dolor, officia nulla qui perspiciatis
+        iure aspernatur deserunt cumque ab iste provident similique, quis
+        recusandae accusantium vel aliquam iusto! Sint reiciendis voluptate
+        praesentium facere, veritatis architecto molestias. Voluptate vitae
+        consequatur earum quibusdam assumenda. Deserunt repudiandae perspiciatis
+        repellendus ratione corporis minus temporibus porro modi! Est ullam
+        dolorem quaerat, libero soluta error alias corrupti natus! Tempora
+        numquam facilis doloremque. Minima saepe, laborum, iste quia earum
+        recusandae obcaecati nostrum itaque quam voluptatum iusto ullam ab. Quae
+        cupiditate nulla, possimus omnis quia corrupti, odio quas tenetur
+        voluptas, ipsam ea eveniet ipsa?
+      </p>
+    </>
+  );
+};
+
+export default Home;
