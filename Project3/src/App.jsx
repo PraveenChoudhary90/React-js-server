bbf45467c5127fe5e4aeb86f9a4111f537196374
praveen choudhary <pc852180@gmail.com>
2024-12-17 23:19:01 +0530

    added Router and Outlet links

--- Project3/src/App.jsx
+++ Project3/src/App.jsx
@@ -1,13 +1,24 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './Layout'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 
 function App() {
   
 
   return (
     <>
-    <Layout/>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Route>
+    </Routes>
+
       
       
     </>
