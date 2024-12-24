d2b57eaa25b73f42705f16f74071a4d4e0c14196
praveen choudhary <pc852180@gmail.com>
2024-12-24 12:32:45 +0530

    Adedd From anf onchange event

--- Fromproject/src/From.jsx
+++ Fromproject/src/From.jsx
@@ -3,14 +3,25 @@ import { useState } from "react";
 const From =()=>{
 
     let [h ,setH]=useState(" ");
+    let [w,setW] = useState(" ");
+    let [value,setvalue] =useState(false)
     function hinput(event){
         console.log(event.target.value);
         setH(event.target.value)
     }
+    function winput(event){
+        console.log(event.target.value);
+        setW(event.target.value);
+
+    }
     return (
         <>
         <h1>my name{h}</h1>
+        <h1>my course {w}</h1>
+        <h1>time is now{value}</h1>
         <input type="text" onChange={hinput} placeholder="enter your name" />
+        <input type="text" onChange={winput} placeholder="enter your course" />
+        <button onChange={()=>setvalue(!value)}>submit</button>
         
         </>
     )
