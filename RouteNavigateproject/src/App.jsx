e54e5a0e75d4c4250161ce7b482ea4dd9c87c24f
praveen choudhary <pc852180@gmail.com>
2025-01-06 12:45:18 +0530

    Usenavigation

--- RouteNavigateproject/src/App.jsx
+++ RouteNavigateproject/src/App.jsx
@@ -1,26 +1,22 @@
-
-import { Route, Routes } from 'react-router-dom'
-import About from './About'
-import './App.css'
-import Contact from './Contact'
-import Home from './Home'
-import Navbar from './Navbar'
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import "./App.css";
+import Contact from "./Contact";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
-
-
   return (
     <>
-    <Routes>
-      <Route>
-        <Route path="/" element = {<Navbar/>} />
-        <Route index element={<Home/>} />
-        <Route path="/about" element={<About/>}/>
-        <Route path="/Contact" element={<Contact/>}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- RouteNavigateproject/src/Navbar.jsx
+++ RouteNavigateproject/src/Navbar.jsx
@@ -1,19 +1,26 @@
 import { Link, Outlet } from "react-router-dom";
 
-const Navbar = ()=>{
-    return(
-        <>
-        <div>
+const Navbar = () => {
+  return (
+    <>
+      <div>
         <ul>
-            <li>
-                <Link to="/">Home</Link></li>
-               <li> <Link to="/about">About</Link></li>
-                <li><Link to="/contact" >Contact</Link>
-            </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            
+            <Link to="/about">About</Link>
+          </li>
+          {/* <li>
+            <Link to="/contact">Contact</Link>
+          </li> */}
         </ul>
-        <Outlet/>
-        </div>
-        </>
-    )
+      </div>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 export default Navbar;
