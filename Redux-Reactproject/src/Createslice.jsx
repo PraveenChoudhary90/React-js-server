c76ad5f5b4db54a8d68643acef7845ab39eac429
praveen choudhary <pc852180@gmail.com>
2025-01-20 12:40:18 +0530

    Redux laibrery

---
+++ Redux-Reactproject/src/Createslice.jsx
@@ -0,0 +1,17 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const counter = createSlice({
+    name:"counter",
+    initialState:{value:0},
+    reducers:{
+        inc : (state)=>{state.value+=1},
+        dec : (state)=>{state.value+=1}
+    }
+})
+
+//declare the object
+//reducers --->store hote hai
+//action
+
+export const {inc,dec}=counter.actions;
+export default counter.reducers;
